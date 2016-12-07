5c44f09f1fe2927156a93d6f086a130ea2c5d71b
Andy Nguyen <andrew.nguyen@colorado.edu>
2016-12-07 02:38:47 -0700

    Pull in correct swatches for front end display. Hook into and display template on front end.

--- lib/class.gabriel.php
+++ lib/class.gabriel.php
@@ -6,7 +6,7 @@ if( !class_exists('base_plugin') )
 
 
 class gabriel extends base_plugin {
-	public $slug = 'custom_palette';
+	public $slug = 'simple';
 	public $object_types = array('post', 'page');
 
     protected function __construct() {
@@ -21,16 +21,19 @@ class gabriel extends base_plugin {
 
 	    # Back end / Custom product type
 	    add_action( 'admin_enqueue_scripts', array( $this, 'admin_javascript' ), 10, 1 );
-	    add_action( 'plugins_loaded', array( $this, 'register_custom_palette_product_type' ) );
+	    #add_action( 'plugins_loaded', array( $this, 'register_custom_palette_product_type' ) );
 
-	    add_filter( 'product_type_selector', array( $this, 'add_custom_palette_product' ) );
+	    #add_filter( 'product_type_selector', array( $this, 'add_custom_palette_product' ) );
 	    add_action( 'admin_footer', array( $this, 'custom_palette_custom_js' ) );
 	    add_filter( 'woocommerce_product_data_tabs', array( $this, 'custom_product_tabs' ) );
 	    add_action( 'woocommerce_product_data_panels', array( $this, 'custom_palette_options_product_tab_content' ) );
-
 	    add_action( 'woocommerce_process_product_meta_custom_palette', array( $this, 'save_custom_palette_option_field'  ) );
-
 	    add_filter( 'woocommerce_product_data_tabs', array( $this, 'hide_attributes_data_panel' ) );
+
+
+
+	    wp_register_script( 'custom-palette', plugins_url( 'js/custom-palette.js', dirname(__FILE__) ), array( 'jquery-ui-core', 'jquery-ui-tabs' ), '1.0', true );
+	    wp_register_style( 'custom-palette', plugins_url( 'css/custom-palette.css', dirname(__FILE__) ) );
 
     }
 
@@ -57,7 +60,6 @@ class gabriel extends base_plugin {
 
 
 
-
 	/*************************************************
 	 * Modifies front end display of custom palette product
 	 *************************************************/
@@ -71,15 +73,67 @@ class gabriel extends base_plugin {
 				#remove_action( 'woocommerce_before_single_product_summary', 'woocommerce_show_product_images', 20 );
 				#add_action( 'woocommerce_before_single_product_summary', array ( $this, 'custom_palette_template' ), 30 );
 				remove_all_actions( 'woocommerce_product_thumbnails' );
+
 				remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
 				add_action( 'woocommerce_single_product_summary', array( $this, 'woocommerce_template_single_excerpt' ), 20 );
-				wp_enqueue_style( 'custom-palette-css', plugins_url( 'css/custom-palette.css', dirname(__FILE__) ) );
+				add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_custom_palette_scripts' ) );
+
+
+				remove_action( 'woocommerce_product_thumbnails', 'woocommerce_show_product_thumbnails', 20 );
+				#add_action( 'woocommerce_product_thumbnails', array( $this, 'woocommerce_show_product_thumbnails', 20 ) );
 			}
 		}
 	}
 
+
+	public function enqueue_custom_palette_scripts() {
+		wp_enqueue_style( 'custom-palette' );
+		wp_enqueue_script( 'custom-palette' );
+    }
+
+
 	public function woocommerce_template_single_excerpt() {
-		$this->get_template('palette-colors');
+		$this->get_template('palette-main');
+    }
+
+
+    # remove
+	public function woocommerce_show_product_thumbnails() {
+		$this->get_wc_template('single-product/product-thumbnails.php');
+		#wc_get_template( 'single-product/product-thumbnails.php' );
+	}
+
+
+	public function woocommerce_display_swatches( $product_id ) {
+
+		$product_palette_id = get_post_meta($product_id, '_palette_product_id', true);
+		$product_palette = new WC_Product_Variable( $product_palette_id );
+
+		$get_variations = sizeof( $product_palette->get_children() ) <= apply_filters( 'woocommerce_ajax_variation_threshold', 30, $product_palette );
+
+
+		$attributes = $product_palette->get_variation_attributes();
+		$attribute = $attribute_keys = array_shift(array_keys( $attributes ));
+
+
+        // Get terms if this is a taxonomy - ordered. We need the names too.
+		$terms = wc_get_product_terms( $product_palette->id, $attribute, array('fields' => 'all') );
+		$config = new WC_Swatches_Attribute_Configuration_Object( $product_palette, $attribute );
+
+		foreach ( $terms as $term ) {
+			if ( in_array( $term->slug, $attributes[$attribute] ) ) {
+				if ( $config->get_type() == 'term_options' ) {
+					$swatch_term = new WC_Swatch_Term( $config, $term->term_id, $attribute, $args['selected'] == $term->slug, $config->get_size() );
+				} elseif ( $config->get_type() == 'product_custom' ) {
+					$swatch_term = new WC_Product_Swatch_Term( $config, $term->term_id, $attribute, $args['selected'] == $term->slug, $config->get_size() );
+				}
+
+				do_action( 'woocommerce_swatches_before_picker_item', $swatch_term );
+				echo $swatch_term->get_output();
+				do_action( 'woocommerce_swatches_after_picker_item', $swatch_term );
+			}
+		}
+
     }
 
 
@@ -156,11 +210,11 @@ class gabriel extends base_plugin {
 
 		echo '<div id="' . $this->slug . '_options" class="panel woocommerce_options_panel">';
 		echo '<div class="options_group">';
-
+		$palette_product_id = get_post_meta($post->ID, '_palette_product_id', true);
 		woocommerce_wp_select( array(
 			'id' 		=> '_palette_product_id',
-			'label' 	=> __( 'Product to pull color palette from:', 'woocommerce' ),
-            'value'     => 'the value to be selected',
+			'label' 	=> __( 'Product to pull colors from:', 'woocommerce' ),
+            'value'     => $palette_product_id,
             'options'   => $this->get_all_wc_products()
 		) );
 
@@ -215,6 +269,18 @@ class gabriel extends base_plugin {
 	 * WooCommerce functions
 	 *************************************************/
 
+	public function add_custom_product() {
+
+		global $woocommerce;
+		$variations = array(
+			'color'     => 'green',
+			'palette 1' => 'color 1',
+			'palette 2' => 'color 2',
+			'palette 3' => 'color 3',
+		);
+		#$woocommerce->cart->add_to_cart( 16661, 1, 0, $variations, array() );
+
+    }
 
 	public function get_all_wc_products() {
 
@@ -227,8 +293,8 @@ class gabriel extends base_plugin {
 
 		$return = array( '' => '' );
 
-		$loop = new WP_Query( $args );
-		foreach( $loop->get_posts() as $post ) {
+		$query = new WP_Query( $args );
+		foreach( $query->get_posts() as $post ) {
 		    $return[$post->ID] = $post->post_title;
         }
         wp_reset_postdata();
@@ -255,6 +321,12 @@ class gabriel extends base_plugin {
 		else
 			get_template_part( 'templates/' . $file );
 	}
+
+
+	public function get_wc_template( $file ) {
+
+		wc_get_template( '/' . $file . '.php',$args = array(), $template_path = '', dirname( __FILE__ ) . '/../templates/woocommerce/' );
+    }
 
 
 }
---
+++ templates/palette-main.php
@@ -0,0 +1,48 @@
+<?php
+if ( ! defined( 'ABSPATH' ) )
+	exit;
+
+global $post, $product;
+
+$images = '';
+
+
+$product_palette_id = get_post_meta($post->ID, '_palette_product_id', true);
+$product_palette = new WC_Product_Variable( $product_palette_id );
+
+
+/*
+$get_variations = sizeof( $product_palette->get_children() ) <= apply_filters( 'woocommerce_ajax_variation_threshold', 30, $product );
+// Load the template
+$product = $product_palette;
+wc_get_template( 'single-product/add-to-cart/variable.php', array(
+	'available_variations' => $get_variations ? $product_palette->get_available_variations() : false,
+	'attributes'           => $product_palette->get_variation_attributes(),
+	'selected_attributes'  => $product_palette->get_variation_default_attributes(),
+    'product' => $product_palette
+) );
+*/
+
+
+
+
+$gabriel = gabriel::get_instance();
+
+$max_colors = 4;
+?>
+
+<div class="custom-palette-colors">
+    <ul>
+    <?php foreach( range(1, $max_colors) as $i ) { ?>
+        <li><a href="#color-<?php echo $i; ?>">Color <?php echo $i; ?></a></li>
+    <?php } ?>
+    </ul>
+
+	<?php foreach( range(1, $max_colors) as $i ) { ?>
+    <div id="color-<?php echo $i; ?>">
+        <?php
+        $gabriel->woocommerce_display_swatches( $product->id );
+        ?>
+    </div>
+    <?php } ?>
+</div>
