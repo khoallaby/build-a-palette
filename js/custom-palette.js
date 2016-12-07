1088f09491275493f39272908e0895a7ece445f4
Andy Nguyen <andrew.nguyen@colorado.edu>
2016-12-07 02:39:22 -0700

    css and js updates for front end tabs

---
+++ js/custom-palette.js
@@ -0,0 +1,7 @@
+(function($){
+    $(document).ready(function() {
+        $( ".custom-palette-colors" ).tabs();
+        console.log('weeeeeeeeeeeeeeeee');
+        console.log( $( ".custom_palette-colors" ));
+    });
+})(jQuery);
